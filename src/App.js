54b3fc51cac56ad24e7a2ce75c9f00681527c0d1
Philip Boardman <dustykeyboard@outlook.com.au>
2022-03-11 15:57:51 +1100

    Remove unused Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { supabase } from "./supabaseClient";
 import Auth from "./components/Auth";
 import Nav from "./components/Nav";
