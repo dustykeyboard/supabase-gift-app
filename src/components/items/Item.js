c4aa17ae4845b6cca3cc245b4efc6a195e94d8f8
Philip Boardman <dustykeyboard@outlook.com.au>
2022-03-12 10:22:33 +1100

    Add unchecked icon, prevent iphone zooming

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,6 +1,6 @@
 const Item = ({ item, handleToggle, handleEdit, handleDelete }) => (
   <li>
-    <div className={item.taken ? 'icon done' : 'icon'} onClick={() => handleToggle(item)} />
+    <div className={item.taken ? 'icon done' : 'icon unchecked'} onClick={() => handleToggle(item)} />
 
     {item.link ? (
       <a href={item.link} target="_blank" rel="noreferrer">
